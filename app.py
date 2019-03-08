522aafbddc28c7bfcc5669a45f0038cb11a8b7b1
C Daniel <cameronspdaniel@gmail.com>
2019-03-08 12:48:25 -0800

    folder architecture ready for heroku deployment

---
+++ app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import data_retrieval as dr
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+   # """Return the homepage."""
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
